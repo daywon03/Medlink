e317155cb859b18ee31ac6308de7fe2bf286b1f9
lucky <luc.macabrey@gmail.com>
2025-12-12 17:35:35 +0100

    chore: resolve stash conflict in app.module.ts (merge patients and elevenlabs services)

--- apps/apip/src/app.module.ts
+++ apps/apip/src/app.module.ts
@@ -4,13 +4,24 @@ import { AppService } from './app.service';
 import { TranscriptionGateway } from './ws/transcription.gateway';
 import { SupabaseService } from './supabase/supabase.service';
 import { ElevenLabsService } from './elevenlabs/elevenlabs.service';
+import { ElevenLabsRealtimeService } from './elevenlabs/elevenlabs-realtime.service';
+import { ElizaArmService } from './eliza/eliza-arm.service';
+import { ElevenLabsTTSService } from './elevenlabs/elevenlabs-tts.service';
 import { PatientsController } from './patients/patients.controller';
 import { PatientsService } from './patients/patients.service';
-
 
 @Module({
   imports: [],
   controllers: [AppController, PatientsController],
-  providers: [AppService, TranscriptionGateway, SupabaseService, ElevenLabsService, PatientsService],
+  providers: [
+    AppService,
+    TranscriptionGateway,
+    SupabaseService,
+    ElevenLabsService,
+    ElevenLabsRealtimeService,
+    ElevenLabsTTSService,
+    ElizaArmService,
+    PatientsService,
+  ],
 })
 export class AppModule {}
