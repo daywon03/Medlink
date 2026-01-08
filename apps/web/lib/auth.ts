f42d2c57574707d02c3fa72851f5ce8c0a362869
Ethan Duquenne <duquenne.ethan@gmail.com>
2026-01-08 02:24:04 +0100

    feat(auth): pages login et inscription (pas totalement fini)

---
+++ apps/web/lib/auth.ts
@@ -0,0 +1,21 @@
+import { supabase } from "./supabaseClient";
+
+export async function login(email: string, password: string) {
+  const { data, error } = await supabase.auth.signInWithPassword({
+    email,
+    password,
+  });
+
+  if (error) throw error;
+  return data;
+}
+
+export async function register(email: string, password: string) {
+  const { data, error } = await supabase.auth.signUp({
+    email,
+    password,
+  });
+
+  if (error) throw error;
+  return data;
+}
